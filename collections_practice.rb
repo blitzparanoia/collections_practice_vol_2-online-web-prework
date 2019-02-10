0a3abca2da333da7337773655ea6cae5f8ddaa47
Irene Saez <bluemilk44@gmail.com>
2019-02-10 16:52:16 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -72,7 +72,7 @@ def organize_schools(schools)
       organized_schools[location] << name
     else 
       organized_schools[location] = []
-      organized_schools << name
+      organized_schools[location] << name
     end
   end
   organized_schools
